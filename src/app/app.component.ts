6bebcffedb26eb6f7a7e469016d4a35e74e14593
Kaique Sodre <kaique.sodre@catalde.com>
2022-11-30 17:33:16 -0300

    Added Demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,46 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'css3d';
+  isHolding: boolean = false;
+  previousPosition: { x: number; y: number };
+
+  xRotate: number = 0;
+  yRotate: number = 0;
+
+  cubeColor: string = 'blue';
+
+  constructor() {
+    this.previousPosition = { x: 0, y: 0 };
+  }
+
+  changeColor(colorName: string) {
+    this.cubeColor = colorName;
+  }
+
+  onHold() {
+    this.isHolding = true;
+  }
+
+  onRelease() {
+    this.isHolding = false;
+    this.previousPosition = { x: 0, y: 0 };
+  }
+
+  onDrag(event: any) {
+    if (this.isHolding) {
+      if (this.previousPosition.x == 0 && this.previousPosition.y == 0) {
+        this.previousPosition.x = event.clientX;
+        this.previousPosition.y = event.clientY;
+      }
+
+      this.xRotate -= event.clientY - this.previousPosition.y;
+      this.yRotate += event.clientX - this.previousPosition.x;
+
+      this.previousPosition.x = event.clientX;
+      this.previousPosition.y = event.clientY;
+    }
+  }
 }
